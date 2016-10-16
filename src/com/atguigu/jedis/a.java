c1ae2f5a8591dc301d16f4f346b78a5f94b6d097
liner_0409 <liner_0409@126.com>
2016-10-16 15:27:13 +0800

    aa

--- src/com/atguigu/jedis/a.java
+++ src/com/atguigu/jedis/a.java
@@ -4,9 +4,5 @@ public class a {
 	public static void main(String[] args) {
 		System.out.println("a");
 		System.out.println("b");
-		System.out.println("c");
-		System.out.println("d");
-		System.out.println("e");
-		System.out.println("e2");
 	}
 }
